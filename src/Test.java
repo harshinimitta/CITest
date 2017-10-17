416921d1599c5978ff93f5474ea99ad99d6464af
harshinimitta <sivaharshinimitta@gmail.com>
2017-10-16 20:26:43 -0400

    Edited

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to CI test project");
-		System.out.println("Hello check");
+		System.out.println("Hello check change");
 	}
 
 }
