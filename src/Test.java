eb2700f0024e3734b07c9ec3d314bd90a5946879
harshinimitta <sivaharshinimitta@gmail.com>
2018-02-13 10:38:50 -0500

    Deleted the unnecessary comment

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Welcome to CI test project");
 		System.out.println("Hello check");
 	}
